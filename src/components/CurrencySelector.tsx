09eb55a5bce9a42338e673c51d221f7fb8754288
Konrad Krzeminski <majunia28@gmail.com>
2023-03-10 16:56:18 +0100

    feat: connect CurrencyPanel with CurrencySelector

---
+++ src/components/CurrencySelector.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { currencyProps, selectProps } from "./types";
+import Select from "react-select";
+
+const CurrencySelector: React.FC<selectProps> = (props) => {
+  const defaultCurrency = "USD - dolar amerykański";
+
+  const nameCurrencyArray = props.rates
+    .map((currency) => currency.code + " - " + currency.currency)
+    .map((name) =>
+      name.includes("(") ? name.slice(0, name.indexOf("(") - 1) : name
+    );
+
+  const options = nameCurrencyArray.map(
+    (value) => [{ value: value, label: value }][0]
+  );
+
+  return (
+    <Select
+      options={options}
+      defaultInputValue={defaultCurrency}
+      onChange={(event) => props.onChangeCureency(event?.value)}
+    />
+  );
+};
+
+export default CurrencySelector;
